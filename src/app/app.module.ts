db4e8b16bc9e7131a8cc8273b19750f39734fc8b
corynnk <corynnk0591@gmail.com>
2020-12-14 12:45:55 -0500

    another commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { BannerComponent } from './banner/banner.component';
+import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './home/body/body.component';
 import { FormsModule } from '@angular/forms';
+import { BodyComponent } from './home/body/body.component';
 
 @NgModule({
   declarations: [
