9870bd3e48018bd70de9006bb0771184ffe4ba55
corynnk <corynnk0591@gmail.com>
2020-12-15 13:12:14 -0500

    added more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +8,18 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { BodyComponent } from './home/body/body.component';
+import { AboutComponent } from './home/about/about.component';
+import { ResumeComponent } from './home/resume/resume.component';
+import { ContactComponent } from './home/contact/contact.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'resume', component: ResumeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +28,10 @@ import { BodyComponent } from './home/body/body.component';
     BannerComponent,
     SidebarComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    AboutComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
